966df83d3b6bc7aad48650c17a220d4f94a52cd0
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-04-24 11:49:24 +0300

    install axios, add questions component

---
+++ client/src/components/Questions.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Category {
+  _id: string;
+  categoryName: string;
+  description: string;
+  questions: string[];
+  createdOn: Date;
+  createdBy: string;
+  categoryStatus: boolean;
+}
+
+const Questions = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  /* const [questions, setQuestions] = useState([]); */
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://exove.onrender.com/api/category",
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(data);
+      setCategories(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  /* useEffect(() => {
+    axios
+      .get("https://exove.onrender.com/api/question")
+      .then((data) => {
+        console.log(data.data);
+        setQuestions(data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []); */
+
+  return (
+    <div>
+      {categories.map((category) => (
+        <h2 key={category._id}>{category.categoryName}</h2>
+      ))}
+    </div>
+  );
+};
+
+export default Questions;
