9100f33223976046549f4c9ada499741edc25236
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-05-03 13:45:40 +0300

    improve styling on admin nav

--- client/src/components/AdminNav/AdminNav.tsx
+++ client/src/components/AdminNav/AdminNav.tsx
@@ -7,27 +7,53 @@ import styles from "./AdminNav.module.css";
 //Translations
 import "../../translations/i18next";
 import { useTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
 
 const AdminNav = () => {
   const { t } = useTranslation(["header"]);
   return (
     <nav className={styles.nav}>
       <ul className={styles.ul}>
-        <li className={styles.li}>
-          <a className={styles.a} href="/dashboard">
-            {t("dashboard")}
-          </a>
-        </li>
-        <li className={styles.li}>
-          <a className={styles.a} href="/template">
-            {t("templates")}
-          </a>
-        </li>
-        <li className={styles.li}>
-          <a className={styles.a} href="/dashboard">
-            {t("userView")}
-          </a>
-        </li>
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending
+              ? `${styles.navlink} ${styles.pending}`
+              : isActive
+              ? `${styles.navlink} ${styles.active}`
+              : styles.navlink
+          }
+          to="/dashboard"
+        >
+          <li className={styles.li}>{t("dashboard")}</li>
+        </NavLink>
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending
+              ? `${styles.navlink} ${styles.pending}`
+              : isActive
+              ? `${styles.navlink} ${styles.active}`
+              : styles.navlink
+          }
+          to="/template"
+        >
+          <li className={styles.li}>{t("templates")}</li>
+        </NavLink>
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending
+              ? `${styles.navlink} ${styles.pending}`
+              : isActive
+              ? `${styles.navlink} ${styles.active}`
+              : styles.navlink
+          }
+          to="/dashboard"
+        >
+          <li className={styles.li}>
+            <a className={styles.a} href="/dashboard">
+              {t("userView")}
+            </a>
+          </li>
+        </NavLink>
       </ul>
     </nav>
   );
