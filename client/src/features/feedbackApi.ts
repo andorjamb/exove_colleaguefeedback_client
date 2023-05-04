8ee5317d79d54914c340b5dd0dea58183ca5558b
Anna Petelin <andorjamb@protonmail.com>
2023-05-04 19:43:26 +0300

    add basic email function for testing

--- client/src/features/feedbackApi.ts
+++ client/src/features/feedbackApi.ts
@@ -3,22 +3,24 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 //Types
 import { IFeedback } from "../types/feedback";
 
-const serverApi = process.env.REACT_APP_SERVER_API;
-const devServerUrl = "http://localhost:4000/";
+//const serverApi = process.env.REACT_APP_SERVER_API;
+const serverApi = "https://exove.vercel.app/api/";
+//const serverApi = "http://localhost:4000/"
 
 export const feedbackApi = createApi({
   reducerPath: "feedbackApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://exove.vercel.app/api/",
+    baseUrl: serverApi,
     prepareHeaders(headers) {
       return headers;
     },
     credentials: "include",
   }),
-  tagTypes: ["Feedback"],
+  tagTypes: ["Feedbacks"],
   endpoints: (builder) => ({
     getAllFeedbacks: builder.query<IFeedback[], void>({
       query: () => "feedback",
+      providesTags: ["Feedbacks"],
     }),
     getFeedbackById: builder.query<IFeedback, string>({
       query: (id) => `feedback/${id}`,
