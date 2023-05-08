0147f0b07e3039836e87c2a0972a3c338f3b078e
jessemwangi <jessejzee@gmail.com>
2023-05-09 01:04:21 +0300

    css to align horizontal line

--- client/src/components/FeedbackForm/RangeQuestions.tsx
+++ client/src/components/FeedbackForm/RangeQuestions.tsx
@@ -13,18 +13,54 @@ const RangeQuestions = ({ question }: question) => {
     
     console.log(value)
     const divStyle = {
+      
         
     }
     const getGradientStyle = () => {
         if (value === -1) {
           return { background: "linear-gradient(to right, #d5d7d5d1, #d5d7d5d1, #d5d7d5d1)" };
-        } else {
+        } else if(value ===0){
             
-          const percentage = ((value ) / 5) * 100;
+          const percentage = ((value  ) / 5) * 100;
           return {
-            background: `linear-gradient(to right, green, yellow ${percentage}%, grey ${percentage}%)`,
+            background: `linear-gradient(to right, #4BEBDD ,#A567CC ${percentage}%, grey ${percentage}%)`,
           };
         }
+        else if(value ===1){
+            
+            const percentage = ((value ) / 5) * 100;
+            return {
+              background: `linear-gradient(to right, #4BEBDD , #A567CC ${percentage}%, grey ${percentage}%)`,
+            };
+        }
+        else if(value ===2){
+            
+            const percentage = ((value ) / 5) * 100;
+            return {
+              background: `linear-gradient(to right, #4BEBDD , #A567CC ,#DF2C9F ${percentage}%, grey ${percentage}%)`,
+            };
+        }
+        else if(value ===3){
+            
+            const percentage = ((value ) / 5) * 100;
+            return {
+              background: `linear-gradient(to right, #4BEBDD , #A567CC ,#DF2C9F, #F85B6B ${percentage}%, grey ${percentage}%)`,
+            };
+        }
+        else if(value ===4){
+            
+            const percentage = ((value ) / 5) * 100;
+            return {
+              background: `linear-gradient(to right, #4BEBDD , #A567CC ,#DF2C9F, #F85B6B, #FFF94D ${percentage}%, grey ${percentage}%)`,
+            };
+        }
+        else if(value ===5){
+            
+            const percentage = ((value ) / 5) * 100;
+            return {
+              background: `linear-gradient(to right, #4BEBDD , #A567CC ,#DF2C9F, #F85B6B , #FFF94D  ${percentage}%)`,
+            };
+          }
       };
     
     // background: `linear-gradient(to right, "#4BEBDD", "#A567CC", "#DF2C9F", "#F85B6B", "#FFF94D", ${percentage}%, grey ${percentage}%)`
@@ -74,9 +110,12 @@ const RangeQuestions = ({ question }: question) => {
     };
 
     return (
-        <div>
+        <div style={{width:"100%", "position": "relative", display:"flex", "justifyContent":"center",flexDirection:"column"}}> 
             <label>{question}</label>
-            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', ...getGradientStyle()}}>
+           
+
+            <div style={{flexDirection: 'row', justifyContent: 'space-around', display: 'flex', ...getGradientStyle()}}>
+         
                 
                     {[1, 2, 3, 4, 5].map((index) => renderRadio(index))}
                    
