3cf1eb6fa424fda3a7fee97db89ab1cca252670e
jessemwangi <jessejzee@gmail.com>
2023-05-11 18:44:38 +0300

    format using pretty

--- client/src/components/FeedbackForm/FeedbackForm.tsx
+++ client/src/components/FeedbackForm/FeedbackForm.tsx
@@ -3,8 +3,8 @@ import style from "./FeedbackForm.module.css";
 import StringQuestions from "./StringQuestions";
 import RangeQuestions from "./RangeQuestions";
 import BoleanQuestions from "./BoleanQuestions";
-import {useGetActiveTemplateQuery} from "../../features/templateApi";
-import {  ITemplate } from "../../types/template";
+import { useGetActiveTemplateQuery } from "../../features/templateApi";
+import { ITemplate } from "../../types/template";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../app/store";
 import { IFCategory, IFeedback } from "../../types/feedback";
@@ -15,182 +15,159 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const FeedbackForm = () => {
-  
   const dispatch = useDispatch<AppDispatch>();
   const { data } = useGetActiveTemplateQuery() || [];
 
-  const [loadigState, setLoadingState] = useState<boolean>(true)
-  const [qTemplate, setActiveTmpt] = useState<ITemplate>()
-  const [userInfo, setUserInfo] = useState<loggedInUser>()
-  const {feedback} = useSelector((state: any) => state.feedback);
+  const [loadigState, setLoadingState] = useState<boolean>(true);
+  const [qTemplate, setActiveTmpt] = useState<ITemplate>();
+  const [userInfo, setUserInfo] = useState<loggedInUser>();
+  const { feedback } = useSelector((state: any) => state.feedback);
   const navigate = useNavigate();
+  const [language, setLang] = useState<string>("Eng");
 
   useEffect(() => {
     if (data) {
-     
-      const categories: IFCategory[] = []
-       
-      data.categories.forEach(cat => {
+      const categories: IFCategory[] = [];
+
+      data.categories.forEach((cat) => {
         const cate: IFCategory = {
           category: cat.category._id,
-          questions:[]
-        }
-        categories.push(cate)
+          questions: [],
+        };
+        categories.push(cate);
       });
       const newFeedbacks: IFeedback = {
-        template: data._id, 
-        requestpicksId: 'da3040ce-43e9-4d2b-89a1-cefe27563492',
-        feedbackTo: 'einstein',
-        progress: 'started',
+        template: data._id,
+        requestpicksId: "da3040ce-43e9-4d2b-89a1-cefe27563492",
+        feedbackTo: "einstein",
+        progress: "started",
         responseDateLog: [new Date().toISOString()],
         categories: categories,
         roleLevel: 5,
-        userId:userInfo?.uid,
-      }
+        userId: userInfo?.uid,
+      };
       dispatch(newfeedback(newFeedbacks));
-      setActiveTmpt(data)
-      
-      setLoadingState(false)
+      setActiveTmpt(data);
+
+      setLoadingState(false);
     } else {
-      setLoadingState(true)
+      setLoadingState(true);
     }
-  
-  }, [data, dispatch, userInfo?.uid])
-   
-  const [language, setLang] = useState<string>('Eng')
+  }, [data, dispatch, userInfo?.uid]);
 
-  const handleSubmitFeedBack = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault()
-   
+  const handleSubmitFeedBack = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
     try {
-
-      const url = 'https://exove.vercel.app/api/feedback/da3040ce-43e9-4d2b-89a1-cefe27563492'
-      const { data } = await axios.post(url,{...feedback} , { withCredentials: true })
-      console.log(data)
-  alert(data)
-    } catch (error) {
-    
-
-    }
-  }
+      const url =
+        "https://exove.vercel.app/api/feedback/da3040ce-43e9-4d2b-89a1-cefe27563492";
+      const { data } = await axios.post(
+        url,
+        { ...feedback },
+        { withCredentials: true }
+      );
+      console.log(data);
+      alert(data);
+    } catch (error) {}
+  };
   useEffect(() => {
     const getUser = async () => {
       try {
-        setLoadingState(true)
-      const userDetails: loggedInUser = await getSecureUserUid()
+        setLoadingState(true);
+        const userDetails: loggedInUser = await getSecureUserUid();
         if (userDetails) {
-          
-          setUserInfo(userDetails)
+          setUserInfo(userDetails);
+        } else {
+          navigate("/login");
         }
-        else {
-          navigate('/login')
-        }
-        setLoadingState(false)
-    } catch (error) {
-        console.log(error)
-        setLoadingState(false)
-    }
-  
-  }
-    getUser()
-  },[navigate])
+        setLoadingState(false);
+      } catch (error) {
+        console.log(error);
+        setLoadingState(false);
+      }
+    };
+    getUser();
+  }, [navigate]);
 
   return (
-
-
     <div className={style.main}>
       <div className={style.user} style={{}}>
-        <h1 className={style.header}>{ qTemplate?.templateTitle}</h1>
-        <h2 className={style.username}>Moi {userInfo ? userInfo.displayName : 'Guest'}</h2>
-
+        <h1 className={style.header}>{qTemplate?.templateTitle}</h1>
+        <h2 className={style.username}>
+          Moi {userInfo ? userInfo.displayName : "Guest"}
+        </h2>
       </div>
 
       <h3 className={style.instructionsTitle}>Instruction</h3>
 
-  
-      <p className={style.instructions}>
-        {qTemplate?.instructions}
-      </p>
+      <p className={style.instructions}>{qTemplate?.instructions}</p>
       <>
-        {
-        loadigState ? (
+        {loadigState ? (
           <>
             <h1>Getting Data .........</h1>
-        </>) :
-          (
-          <div className = {style.questionContainer}>
-        {qTemplate && qTemplate.categories?.map(
-          (cat) => (
-            <div className={style.catQuest} key={cat.category._id} >
-              <h2>{cat.category.categoryName}</h2>
+          </>
+        ) : (
+          <div className={style.questionContainer}>
+            {qTemplate &&
+              qTemplate.categories?.map((cat) => (
+                <div className={style.catQuest} key={cat.category._id}>
+                  <h2>{cat.category.categoryName}</h2>
 
-              {
-                cat.category.questions.map(
-                  (quiz) => (
+                  {cat.category.questions.map((quiz) => (
                     <div key={quiz._id}>
-                      {quiz.type.toLowerCase() === 'string' ? (
+                      {quiz.type.toLowerCase() === "string" ? (
                         <>
-                        <StringQuestions
-                          
-                          key={quiz._id}
-                          category={quiz.category}
-
-                          questions= {
-                              quiz.question.find(quiz => quiz.lang === language)!
-                              
+                          <StringQuestions
+                            key={quiz._id}
+                            category={quiz.category}
+                            questions={
+                              quiz.question.find(
+                                (quiz) => quiz.lang === language
+                              )!
                             }
-                      
-                        />
+                          />
                         </>
-                      ) : quiz.type.toLowerCase() === 'number' ? (
+                      ) : quiz.type.toLowerCase() === "number" ? (
                         <RangeQuestions
                           key={quiz._id}
                           category={quiz.category}
-
-                          questions= {
-                              quiz.question.find(quiz => quiz.lang === language)!
-                              
-                            }
+                          questions={
+                            quiz.question.find(
+                              (quiz) => quiz.lang === language
+                            )!
+                          }
                         />
-                      ) :
-                        (
-                          <BoleanQuestions
-                            key={quiz._id}
-                            category={quiz.category}
-
-                          questions= {
-                              quiz.question.find(quiz => quiz.lang === language)!
-                              
-                            }
-                          />
-                        )
-                      }
+                      ) : (
+                        <BoleanQuestions
+                          key={quiz._id}
+                          category={quiz.category}
+                          questions={
+                            quiz.question.find(
+                              (quiz) => quiz.lang === language
+                            )!
+                          }
+                        />
+                      )}
                     </div>
-                  )
-                )
-              }
-            </div>
-      
-          )
-        )
-        }
-      </div>
-  )
+                  ))}
+                </div>
+              ))}
+          </div>
+        )}
+      </>
 
-  }
-   </>
-
-<div className={style.formElements}>
-<button
+      <div className={style.formElements}>
+        <button
           className={[style.button, style.loginButton].join(" ")}
-          onClick={(e)=> handleSubmitFeedBack(e)}
->
-Submit
+          onClick={(e) => handleSubmitFeedBack(e)}
+        >
+          Submit
         </button>
-        
-</div>
-  </div>
-  )
+      </div>
+    </div>
+  );
 };
 
 export default FeedbackForm;
