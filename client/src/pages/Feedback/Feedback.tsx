a94e85e5b174043aa8819e8499e8e7844d4fc045
jessemwangi <jessejzee@gmail.com>
2023-05-17 22:56:26 +0300

    notification function for sending emails

--- client/src/pages/Feedback/Feedback.tsx
+++ client/src/pages/Feedback/Feedback.tsx
@@ -2,10 +2,28 @@ import React from "react";
 
 //Components
 import FeedbackForm from "../../components/FeedbackForm/FeedbackForm";
+import { functionData, newEmailDetails } from "../../types/notification";
+import { sendNotification } from "../../functions/notification";
 
 const Feedback = () => {
+  const sendEmail = async () => {
+    const details:functionData = {
+      link: 'string',
+      emailTo: 'string',
+      from_name: 'string',
+      messageBody: 'string',
+      applicationId: 'string',
+      entityname: 'string',
+      subject: 'string',
+      reply_to:'string',
+      
+    }
+    const sendemail = sendNotification(details)
+    console.log('sendemail ****************',sendemail)
+  }
   return (
     <div>
+      <button onClick={sendEmail}>send email</button>
       <FeedbackForm />
     </div>
   );
