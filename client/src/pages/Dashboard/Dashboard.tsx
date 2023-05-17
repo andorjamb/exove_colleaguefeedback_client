b5bf57862c37ab94d982c1a2f3749b650e65df82
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-05-17 14:09:54 +0300

    fix refresh issue

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,27 +1,51 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-//Styling
-import styles from "./Dashboard.module.css";
 
 //Pages and Components
 import DashboardAdmin from "../../components/DashboardAdmin/DashboardAdmin";
 import DashboardUser from "../../components/DashboardUser/DashboardUser";
 import DashboardManager from "../../components/DashboardManager/DashboardManager";
 
+// Types
+import { loggedInUser } from "../../types/users";
+
+// Functions
+import { getSecureUserUid } from "../../functions/secureUser";
+
+//Styling
+import styles from "./Dashboard.module.css";
+
 const Dashboard = () => {
-  const loggedIn = useSelector((state: any) => state.auth.loggedIn);
-  const isAdmin = useSelector((state: any) => state.auth.isAdmin);
+  const [currentUserInfo, setCurrentUserInfo] = useState<loggedInUser>();
+  const [isHR, setIsHR] = useState<boolean | undefined>();
+
+  const getUserInfo = async () => {
+    const userDetails: loggedInUser = await getSecureUserUid();
+    setCurrentUserInfo(userDetails);
+  };
 
   useEffect(() => {
-    //debugging
-    console.log("loggedIn:", loggedIn);
-    console.log("isAdmin:", isAdmin);
+    try {
+      getUserInfo();
+    } catch (err) {
+      console.log("error getting user", err);
+    }
   }, []);
+
+  useEffect(() => {
+    // When user info is ready, determine if user is HR
+    if (!currentUserInfo) {
+      return;
+    }
+    if (currentUserInfo.roleLevel < 3) setIsHR(true);
+    else setIsHR(false);
+  }, [currentUserInfo]);
+
+  if (!currentUserInfo) return <p>user info loading...</p>;
+
   return (
     <div className={styles.container}>
       <h1>Dashboard</h1>
-      {isAdmin ? <DashboardAdmin /> : <DashboardUser />}
+      {isHR ? <DashboardAdmin /> : <DashboardUser />}
     </div>
   );
 };
